0f6a40b96a7e87ee5b79c529ed4181a9777175da
beatiz-sait07 <beatriz.s.gobira@gmail.com>
2022-08-09 15:41:40 -0400

    melhorando

--- bubble.c
+++ bubble.c
@@ -1,7 +1,6 @@
 #include"bubble.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 
 int cont_varredura = 0;
 int cont_trocas = 0;
@@ -21,11 +20,13 @@ void bubble(int vector[], int size)
         for(int j=0; j<size-1; j++){
 
             if(vector[j] > vector[j+1]){
+
                 swap(&vector[j], &vector[j+1]);
                 cont_varredura = vector[j];
                 if (cont_varredura == vector[j - 1]) break;
                 cont_trocas += 1;
             }
+            cont_comp += 1;
         }
         cont_varredura += 1;
     }
@@ -37,7 +38,22 @@ int print_novo_vetor(const int *vector, int size)
     for(int i=0; i<size; i++){
         printf("|%d", vector[i]);
     }
-    printf("\no vetor foi analisado: %d\n", cont_varredura);
-    printf("nocorreu: %d trocas dentro do vetor ate ele se organizar\n", cont_trocas);
+    printf("\no vetor foi analisado: %d\n", (cont_varredura - 1));
+    printf("ocorreu: %d trocas dentro do vetor ate ele se organizar\n", cont_trocas);
     printf("quantidade de comparacoes: %d\n", cont_comp);
 }
+
+
+
+/*int print_novo_vetor(const int *vector, int size)
+{
+    FILE *file = fopen("dados bubble_sort.txt", "w");
+
+    if(file == NULL)printf("nao foi possivel criar um arquivo!");
+    for(int i=0; i<size; i++){
+        fprintf(file,"|%d", vector[i]);
+    }
+    fprintf(file, "\n\n\no vetor foi analisado: %d\n", (cont_varredura - 1));
+    fprintf(file,"nocorreu: %d trocas dentro do vetor ate ele se organizar\n", cont_trocas);
+    fprintf(file,"quantidade de comparacoes: %d\n", cont_comp);
+}*/
--- bubble_S.c
+++ bubble_S.c
@@ -1,14 +1,58 @@
 #include"bubble.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <time.h>
+#define T 8
+#define TM 50
+#define TAM 10
+
 
 int main (int argc, char const *argv[])
 {
-    int vetor[8] = {7, 3, 0, 1, 3, 8, 2, 9};
-    printf("|%d", vetor[8]);
-    bubble(vetor,8);
-    print_novo_vetor(vetor, 8);
+    double tp_ex = 0.0;
+    clock_t begin = clock();
+
+    int vector[T] = {7,3,0,1,3,8,2,9};
+
+    bubble(vector,T);
+    print_novo_vetor(vector, T);
+    /*int n;
+
+    
+    printf("escolha as opções a serem rodadas:");
+    printf("1) 100k\n2) 500k\n3) 1M\n");
+
+    switch (n)
+    {
+
+        case 1: printf("rodando um vetor de 100k");
+        {
+            bubble(vector,TAM);
+            print_novo_vetor(vector, T);
+        }
+        break;
+
+        case 2: printf("redando um vetor de 500k"); 
+        {
+            bubble(vector,TAM);
+            print_novo_vetor(vector, TM);
+        }
+        break;
+
+        case 3: printf("rodando um vetor de 1M");
+        {
+            bubble(vector,TAM);
+            print_novo_vetor(vector, 8);
+        }
+        break;
+    
+    default: printf("digite um numero valido!");
+    }*/
+
+    clock_t end = clock();
+    tp_ex += (double)(end - begin)/ CLOCKS_PER_SEC;
+
+    printf("tempo de execusao foi de %fs\n", tp_ex);
 
     return 0;
 }
