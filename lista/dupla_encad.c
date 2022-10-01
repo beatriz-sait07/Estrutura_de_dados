a7d5d49c6916618cfa58205d59de252f2b3e7276
beatriz-sait07 <beatriz.s.gobira@gmail.com>
2022-10-01 19:02:36 -0400

    destroy

--- lista/dupla_encad.c
+++ lista/dupla_encad.c
@@ -31,4 +31,24 @@ List *List_create(){
     list->inicio = NULL;
     list->fim = NULL;
     list->size = 0;
+
+    return list;
+}
+
+bool _isNull(const Lista_dupla *list){
+    return list->size == 0;
+}
+
+void destroy(Lista_dupla **ref_list){
+    List *aux_list = ref_list; //inicia a desalocacao de 
+    
+    Node *p = aux_list->inicio; //usado para desalocar a casa atual
+    Node *aux = NULL; //usado pata não perder a referencia
+
+    while(p != NULL){
+        aux = p;
+        p = p->next;
+        free(aux);
+    }
+    free(aux_list);
 }
