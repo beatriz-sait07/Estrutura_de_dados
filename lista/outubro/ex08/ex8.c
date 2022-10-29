3391527bb72697aba009c8dd28c609721ff87aac
beatriz-sait07 <beatriz.s.gobira@gmail.com>
2022-10-29 13:43:33 -0400

    declaração das estruturas

--- lista/outubro/ex08/ex8.c
+++ lista/outubro/ex08/ex8.c
@@ -2,3 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct cad_inf {
+    char *usuario_cad;
+    char *nome_cad;
+    int *id;
+    int vaga, placa;
+} Info;
+
+typedef struct node_simples {
+    Info *cadastro_cliente;
+    struct node_simples *next;
+    struct node_simples *prev;
+    
+} Node;
+
+typedef struct linkedlist {
+    Node *begin;
+    Node *end;
+    long int size_list;
+} List;
