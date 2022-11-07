03afd3eda019728fb3f24245c796cb835421dfae
beatriz-sait07 <beatriz.s.gobira@gmail.com>
2022-11-07 14:16:19 -0400

    .

--- fila/fila_est.c
+++ fila/fila_est.c
@@ -1,11 +1,13 @@
-#ifndef FILA_EST_H
-#define FILA_EST_H
-#include <stdbool.h>
+#include "fila_est.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct fila_estatica Fila;
+typedef struct fila_estatica {
+    int *data, size;
+    long capacity, begin, end;
+    //size: quantos elementos tem na fila, capacity: quantos elementos cabe na fila
+} Fila;
 
-Fila *fila_estatica(long capacity); //necessaria
-void destroy(Fila **ref_fila);
-
-
-#endif
+Fila *fila_estatica(long capacity){
+    Fila *q = (Fila*)calloc(1, sizeof(Fila));
+}
