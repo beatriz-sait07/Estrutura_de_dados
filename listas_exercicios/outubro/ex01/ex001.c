b6eb7cd567e97f10a21a9e05debf47b855334043
saito <beatriz.s.gobira@gmail.com>
2022-11-17 11:57:21 -0400

    funcoes de create

--- listas_exercicios/outubro/ex01/ex001.c
+++ listas_exercicios/outubro/ex01/ex001.c
@@ -16,3 +16,18 @@ typedef struct _lista_simples{
 bool _isNull(const Lista *list){
     return list->size == 0;
 }
+
+Node_simples *Node_create(int val){
+    Node_simples *node = (Node_simples*)calloc(1, sizeof(Node_simples));
+    node->valor = val;
+    node->next = NULL;
+    return node;
+}
+
+Lista *Lista_create(){
+    Lista *lista = (Lista *)calloc(1, sizeof(Lista));
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->size = 0;
+    return lista;
+}
