a770314abaf96238c898a680885d03a80f357a42
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2022-11-27 16:11:37 -0400

    .

--- listas_exercicios/nov_dez/ex01/ex01.h
+++ listas_exercicios/nov_dez/ex01/ex01.h
@@ -5,13 +5,13 @@
 typedef struct _double_node DoubleNode, Node;
 typedef struct _double_list DoubleList, List;
 
-Node *create_node(int val);
+Node *create_node(int altura, int idade);
 List *create_list();
 
 void destroy_dupla(List **ref_list);
 bool _isNull_dupla(const List *list);
 bool _isFull_dupla(const List *list);
-void add_final_dupla(List *list, int val); 
+void add_final_dupla(List *list, int alt, int idd); 
 void imprimir_lista(const List *list);
 
 #endif
