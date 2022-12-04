60d9afd359058d029a7037bee5fcd33db86b0fdb
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2022-12-04 16:47:11 -0400

    concluido

--- algoritmos/alg I/lista_05/ex01.c
+++ algoritmos/alg I/lista_05/ex01.c
@@ -6,22 +6,7 @@ int main (){
     printf("qual tamanho do array: ");scanf("%d", &tam);
 
     // ja considerado que o usuario entrara com 1<= n <= 100
-    for(i = 0; i < tam; i++){
-        printf("valor para posicao [%d]: ", i);scanf("%d", &val);
-        vetor[i] = val;
 
-        for(j = 0; j <= i-1 ; j++){
-            if(val == vetor[j]){
-                cont = 1;
-                break;
-            }
-        }
-        if(cont == 1){
-            cont = 0;
-            i -= 1;
-            tam -= 1; 
-        } else vetor[i] = val;
-    }
     for(i = 0; i < tam; i++) printf("|%d|", vetor[i]);
                 
     return 0;
