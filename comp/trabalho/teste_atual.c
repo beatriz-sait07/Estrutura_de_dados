2c98d93f395c1b5aa37e30f661c6efa0f3770c27
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-04-02 18:25:41 -0400

    esse sempre vai ser a falha atual

--- comp/trabalho/teste_atual.c
+++ comp/trabalho/teste_atual.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "lista_enc.h"
 
 #define AUTO
 
-void token(Lista **l){
-    
+void token(Lista **l) {
     FILE *arq;
     arq = fopen("tokens.txt", "r");
 
-    if (arq == NULL){
-        printf("erro ao abrir o arquivo!\n");
+    if (arq == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
         exit(1);
     }
+
     int i = 0;
     l[i] = Lista_create();
     char aux = fgetc(arq);
@@ -28,22 +29,160 @@ void token(Lista **l){
         aux = fgetc(arq);
 
     }
+    fclose(arq);
+}
+
+
+char buffer(Lista **l){
+    FILE *arq;
+    arq = fopen("arq.c", "r");
+
+    if (arq == NULL){
+        printf("erro ao abrir o arquivo!\n");
+        exit(1);
+    }
+    char buffer[50];
+    int i=0;
+    char atual;
+    char proximo;
+    
+
+    /*while(1){
+        int aux = fgetc(arq);
+        if (aux == EOF){
+            break;
+        }
+        if (aux != ' ' && aux != '\n' && aux != '\t' && aux != '\r'){
+            in_word = 1;
+            if(!(aux >= 'a' && aux <= 'z' || aux >= 'A' && aux <= 'Z')){
+
+            }
+                
+            buffer[i] = aux;
+            i++;
+        }
+        else if (in_word){ 
+            in_word = 0;
+            for(int j=0; j<i; j++){
+                printf("%c", buffer[j]);
+            }
+            printf("\n");
+            for(int j=0; j<50; j++){
+                buffer[j] = 0;
+            }
+            i=0;
+        }
+    }*/
+
+    while(1){
+        atual = fgetc(arq);
+        proximo = atual;
+        if (proximo == EOF){
+            break;
+        }
+        if (proximo != ' ' && proximo != '\n' && proximo != '\t' && proximo != '\r'){
+            if(!(proximo >= 'a' && proximo <= 'z' || proximo >= 'A' && proximo <= 'Z')){
+                if(!(proximo < '0' || proximo > '9')){
+                    if(proximo == '"'){
+                        while(proximo != '"'){
+                            buffer[i] = proximo;
+                            i++;
+                            atual = fgetc(arq);
+                            proximo = atual;
+                        }
+                    } else {
+                        buffer[i] = proximo;
+                        i++;
+                        atual = fgetc(arq);
+                        proximo = atual;
+                    }
+                } else {
+                    //vetor recebe numero
+                    buffer[i] = proximo;
+                    i++;
+                    atual = fgetc(arq);
+                    proximo = atual;
+                }
+            } else {
+                buffer[i] = proximo;
+                i++;
+                atual = fgetc(arq);
+                proximo = atual;
+            }
+        }
+        else if (atual){ 
+            for(int j=0; j<i; j++){
+                printf("%c", buffer[j]);
+            }
+            printf("\n");
+            for(int j=0; j<50; j++){
+                buffer[j] = 0;
+            }
+            i=0;
+        }
+
+    }
 
     fclose(arq);
 }
 
-void valida_token(Lista **l_princ, Lista *l_token){
+/*void valida_token(Node *node, char *buffer){
+    if(node == NULL){
+        printf("Token invalido!\n");
+        exit(1);
+    }
+    else{
+        if(strcmp(node->letra, buffer) == 0){
+            printf("Token valido!\n");
+        }
+        else{
+            valida_token(node->next, buffer);
+        }
+    }
+}*/
 
+void valida_token(Node *node, char *buffer){
+    ;
+    if(node == NULL){
+        printf("%s -> Token invalido!\n", buffer);
+        exit(1);
+    }
+    else{
+        int i = 0;
+        while (node->letra != '\0' && buffer[i] != '\0') {
+            if (node->letra != buffer[i]) {
+                valida_token(node->next, buffer);
+                return;
+            }
+            i++;
+        }
+        if (node->letra == '\0' && buffer[i] == '\0') {
+            printf(" %s -> Token valido!\n", buffer);
+        } else {
+            valida_token(node->next, buffer);
+        }
+    }
 }
 
 
-int main (){
-    Lista *list[75];
-    token(list);
-    for(int i=0; i<75; i++){
-        print_List(list[i]);
-        printf("\n");
+int main(){
+    int n = 75;
+    Lista **list = (Lista **) malloc(n * sizeof(Lista *));
+
+    if (list == NULL){
+        printf("Erro ao alocar memoria!\n");
+        exit(1);
     }
 
+    token(list);
+
+    /*for(int i=0; i<n; i++){
+        print_List(list[i]);
+        printf("\n");
+    }*/
+    buffer(list);
+
+    //valida_token(list[0]->inicio, "auto");
+    free(list); 
     return 0;
 }
