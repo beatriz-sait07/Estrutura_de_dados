01a35ecb9885fb1f030c54df5298e9173a7b348f
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-06-25 18:23:48 -0400

    renomeando pasta

---
+++ semestre_07/comunicacao-dados/trabalhos/write_html_cgi.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+
+int main() {
+    printf("Content-type: text/html\n\n");
+    printf("<html><body>");
+
+    int serial_port = open("./saida", O_RDONLY);
+    
+    if (serial_port < 0) {
+        printf("Erro ao abrir o dispositivo: %s\n", strerror(errno));
+        printf("</body></html>");
+        return 1;
+    }
+    
+    struct termios tty;
+    if (tcgetattr(serial_port, &tty) != 0) {
+        printf("Erro ao obter os atributos do terminal: %s\n", strerror(errno));
+        close(serial_port);
+        printf("</body></html>");
+        return 1;
+    }
+    
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= CREAD | CLOCAL;
+    
+    tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHONL | ISIG);
+    
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+
+    
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+        printf("Erro ao definir os atributos do terminal: %s\n", strerror(errno));
+        close(serial_port);
+        printf("</body></html>");
+        return 1;
+    }
+    
+    char read_buf[256];
+    memset(read_buf, '\0', sizeof(read_buf));
+    
+    ssize_t num_bytes = read(serial_port, read_buf, sizeof(read_buf) - 1);
+    
+    if (num_bytes < 0) {
+        printf("Erro ao ler do dispositivo: %s\n", strerror(errno));
+        close(serial_port);
+        printf("</body></html>");
+        return 1;
+    }
+    
+    printf("Mensagem recebida: %s<br>", read_buf);
+    
+    close(serial_port);
+    
+    printf("</body></html>");
+
+    return 0;
+}
