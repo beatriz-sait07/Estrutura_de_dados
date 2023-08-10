a11471f1ec7f1e4b5010fe8c41168b72076d6791
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-08-10 17:28:21 -0400

    trabalho 01

--- semestre_08/POO/conta_bancaria.py
+++ semestre_08/POO/conta_bancaria.py
@@ -1,24 +1,50 @@
-class Conta:
+import datetime
+
+class Cliente:
     
-    def __init__(self, titular, numero, saldo, limite):
-        print(f'inicializando a conta {titular}, Bem vindo(a)!')
-        self.titular = titular
-        self.numero = numero
-        self.saldo = saldo
-        self.limite = limite
+    def __init__(self, nome, cpf, data) -> None:
+        self.nome = nome
+        self.cpf = cpf
+        self.data = data
         
-    
-def sacar(saldo, limite, valor):
-    valor = float('quanto voce deseja sacar: R$ ')
-    
-    if valor < (limite + saldo):
-        print('ERRO AO SACAR')
-    else:
-        print('saque R$ {valor:.2f}')
-        saldo = saldo - valor
-    
-    
-def depositar(saldo, valor):
-    
-            
-    
+    def __repr__(self) -> str:
+        return f'{self.nome}, {self.cpf}, {self.data}'
+
+class Conta:
+
+    def __init__(self, agencia, numero, saldo, cliente) -> None:
+        self.agencia = agencia
+        self.numero_conta = numero
+        self.saldo = saldo
+        self.cliente = cliente
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo += valor
+            return True
+        else:
+            return False
+
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor   
+            return True
+        else:
+            return False
+        
+    def transferir(self, valor, destinatario, agencia_dest, conta_dest):
+        if self.sacar(valor):
+            destinatario.depositar(valor)
+            return True
+        else:
+            return False
+
+cliente1 = Cliente("João", "123.456.789-00", '10/08/2023')
+cliente2 = Cliente("Maria", "987.654.321-00", datetime.datetime.now())
+
+conta1 = Conta("001", "12345", 1000, cliente1)
+conta2 = Conta("001", "67890", 500, cliente2)
+
+conta1.transferir(200, conta2, "001", "67890")
+print(conta1.saldo)
+print(conta2.saldo)
