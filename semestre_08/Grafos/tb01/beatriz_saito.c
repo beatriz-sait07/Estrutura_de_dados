1d35bd686c9735bfa2e9fb8ffc8a5ce8ee46ab9d
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-08-26 20:11:36 -0400

    completo!

--- semestre_08/Grafos/tb01/beatriz_saito.c
+++ semestre_08/Grafos/tb01/beatriz_saito.c
@@ -1,8 +1,10 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<stdbool.h>
 
+#define TAM 8
 
 //-------------------------------LISTA ENCADEADA---------------------------------//
 struct node{
@@ -13,6 +15,7 @@ struct lista{
     struct node *begin;
     struct node *end;
     char pal[30];
+    int porcent;
     int size;
 };
 struct node *Node_Create(char val)
@@ -98,38 +101,262 @@ void replace(struct lista **lista) {
             } else {
                 fprintf(stderr, "Erro: Nao foi possivel identificar o estado do vertice %d", i);
             }
-
             aux = aux->next;
         }
-
         i++;
+    }
+}
+//-----------------------------------------CHANCE DE INFECCAO---------------------------------------//
+/*   considerando um individuo sem mascara e saudavel (00), com outras quaisquer combinacoes de estado,
+apos irei realizar a chance de um individuo com mascara e saudavel (10), com outras quaisquer combinacoes
+de estado!         */
+
+int percorre(struct lista *lista){
+    struct node *aux = lista->begin;
+    if(aux->val == '0'){    // 1 coluna
+        aux = aux->next;
+        if(aux->val == '0'){    // 2 coluna
+            aux = aux->next;
+            if(aux->val == '0'){   // 3 coluna
+                aux = aux->next;
+                if(aux->val == '0'){    // 4 coluna
+                    return 0;
+                }else if(aux->val == '1'){  // 4 coluna
+                    return 100;
+                }
+            }else if(aux->val == '1'){  // 3 coluna
+                aux = aux->next;
+                if(aux->val == '0'){    // 4 coluna
+                    return 0;
+                }else if(aux->val == '1'){  // 4 coluna
+                    return 50;
+                }
+            }
+        }
+    }else{      // 1 coluna
+        aux = aux->next;
+        if(aux->val == '0'){    // 2 coluna
+            aux = aux->next;
+            if(aux->val == '0'){   // 3 coluna
+                aux = aux->next;
+                if(aux->val == '0'){    // 4 coluna
+                    return 0;
+                }else if(aux->val == '1'){  // 4 coluna
+                    return 50;
+                }
+            }else if(aux->val == '1'){  // 3 coluna
+                aux = aux->next;
+                if(aux->val == '0'){    // 4 coluna
+                    return 0;
+                }else if(aux->val == '1'){  // 4 coluna
+                    return 25;
+                }
+            }
+        }
+    }
+}
+void atribui_Porcent(struct lista **lista){
+    for(int i=0; i<TAM; i++){
+        int p = percorre(lista[i]);
+        lista[i]->porcent = p;
     }
 }
 
 //-------------------------------------------------MAIN--------------------------------------------//
 int main(){
     
-    struct lista **lista = (struct lista**)malloc(8 * sizeof(struct lista*));
+    struct lista **lista = (struct lista**)malloc(TAM * sizeof(struct lista*));
     leitura_arq(lista);
 
-    // CASO QUEIRA IMPRIMIR A LISTA DE ADJACENCIA DE TOKENS DESCOMENTE A FUNCAO ABAIXO
-    for(int j=0; j<8; j++){
+
+    for(int j=0; j<TAM; j++){
         printf("\n|%d| = ", j);
         print(lista[j]);
-        printf("%s", lista[j]->pal);
+        //printf("%s", lista[j]->pal);
     }
-    printf("\ntransformacao da lista para bool\n");
     replace(lista);
-    for(int j=0; j<8; j++){
+    atribui_Porcent(lista);
+
+    printf("LIsta com booleanos e suas porcentagens de infeccatados: \n");
+    for(int j=0; j<TAM; j++){
         printf("\n|%d| = ", j);
         print(lista[j]);
-        printf("%s", lista[j]->pal);
+        printf(" - %d%%", lista[j]->porcent);
     }
-
-
-    free(lista);
     printf("\n");
     return 0;
 }
+/*void chance_infeccao(struct lista **lista) {
+    if (is_Empty(lista[0])) {
+        printf("Lista vazia!\n");
+        exit(1);
+    } 
+    int i = 0;
+    struct node *aux = lista[i]->begin;
 
+    while(aux != NULL){
+        if(aux->val == '0'){ //msk - 1
+            aux = aux->next;
+            if (aux->val == '0'){ // infc - 2
+                aux = aux->next;
+                if(aux->val == '0'){ // msk - 3
+                    aux = aux->next;
+                    if(aux->val == '0'){ // infc - 4
+                        if(aux->next == NULL){
+                            infec[i] = 0;
+                        } else {
+                            fprintf(stderr, "Erro: ha possibilidades inexistentes para verificacoes!\n");
+                        }
+                    } else{ 
+                        if(aux->next == NULL){
+                            infec[i] = 100;
+                        } else {
+                            fprintf(stderr, "Erro: ha possibilidades inexistentes para verificacoes!\n");
+                        }
+                    }
+                } else { // infc - 3
+                    aux = aux->next;
+                    if(aux->val == '0'){
+                        if(aux->next == NULL){
+                            infec[i] = 0;
+                        } else {
+                            fprintf(stderr, "Erro: ha possibilidades inexistentes para verificacoes!\n");
+                        }
+                    } else{ //infc - 4
+                        if(aux->next == NULL){
+                            infec[i] = 50;
+                        } else {
+                            fprintf(stderr, "Erro: ha possibilidades inexistentes para verificacoes!\n");
+                        }
+                    }
+                }
+            } else { // infc - 2
+                fprintf(stderr, "Erro: individuo sem mascara e infectado!\n");
+            }
+        } else { // msk - 1
+            aux = aux->next;
+            if (aux->val == '0'){ // infc - 2
+                aux = aux->next;
+                if(aux->val == '0'){ //msk
+                    aux = aux->next;
+                    if(aux->val == '0'){ // infc - 4
+                        if(aux->next == NULL){
+                            infec[i] = 0;
+                        } else {
+                            fprintf(stderr, "Erro: ha possibilidades inexistentes para verificacoes!\n");
+                        }
+                    } else{ //msk
+                        if(aux->next == NULL){
+                            infec[i] = 50;
+                        } else {
+                            fprintf(stderr, "Erro: ha possibilidades inexistentes para verificacoes!\n");
+                        }
+                    }
+                } else { // infc - 3
+                }
+            } else { // infc - 2
+                fprintf(stderr, "Erro: individuo com mascara e infectado!\n");
+            }
+        }
+    }
+}*/
 
+/*void chance_infeccao(struct lista **lista) {
+    int i = 0, j = 0;
+    int chance = 0; // 0 -> saudavel, 1 -> infectado
+    int vet[4];
+    struct node *aux = lista[i]->begin;
+    
+    while (aux != NULL) {
+        vet[j] = aux->val;
+        aux = aux->next;
+        j++;
+        
+        if (vet[strlen(vet)]) {
+            if (vet[0] == '0' && vet[1] == '0' && vet[2] == '0' && vet[3] == '0') {
+                chance = 100;
+            } else if (vet[0] == '0' && vet[1] == '0' && vet[2] == '1' && vet[3] == '0') {
+                chance = 101;
+            } else if (vet[0] == '0' && vet[1] == '0' && vet[2] == '1' && vet[3] == '1') {
+                chance = 102;
+            } else if (vet[0] == '0' && vet[1] == '0' && vet[2] == '0' && vet[3] == '1') {
+                chance = 103;
+            } else if (vet[0] == '1' && vet[1] == '0' && vet[2] == '0' && vet[3] == '0') {
+                chance = 104;
+            } else if (vet[0] == '1' && vet[1] == '0' && vet[2] == '1' && vet[3] == '0') {
+                chance = 105;
+            } else if (vet[0] == '1' && vet[1] == '0' && vet[2] == '1' && vet[3] == '1') {
+                chance = 106;
+            } else if (vet[0] == '1' && vet[1] == '0' && vet[2] == '0' && vet[3] == '1') {
+                chance = 107;
+            } else {
+                fprintf(stderr, "Erro: ha possibilidades inexistentes para verificacoes!\n");
+            }
+            infec[i] = chance;
+            i++; // Atualizar o índice
+            j = 0; // Resetar o índice j para o próximo conjunto de valores
+            continue;
+        }
+    }
+}*/
+
+/*void chance_infeccao(struct lista **lista){
+    int i = 0, j = 0;
+    struct node *aux = lista[i]->begin;
+
+    char *vet = (char*)malloc(4 * sizeof(char));
+
+    while(aux != NULL){
+        printf("%d\n", i);
+        if(lista[i]->end == NULL){
+            printf("teste!\n");
+            if(vet[0] == '0' && vet[1] == '0' && vet[2] == '0' && vet[3] == '0'){
+                //infec[i] = 0;
+                lista[i]->porcentagem = 0;
+                printf("vetor: %s \n", vet);
+            } else if(vet[0] == '0' && vet[1] == '0' && vet[2] == '1' && vet[3] == '0'){
+                //infec[i] = 100;
+                printf("vetor: %s \n", vet);
+                lista[i]->porcentagem = 0;
+            } else if(vet[0] == '0' && vet[1] == '0' && vet[2] == '1' && vet[3] == '1'){
+                //infec[i] = 50;
+                printf("vetor: %s \n", vet);
+                lista[i]->porcentagem = 50;
+                printf("vetor: %s \n", vet);
+            } else if(vet[0] == '0' && vet[1] == '0' && vet[2] == '0' && vet[3] == '1'){
+                //infec[i] = 100;
+                printf("vetor: %s \n", vet);
+                lista[i]->porcentagem = 100;
+            } else if(vet[0] == '1' && vet[1] == '0' && vet[2] == '0' && vet[3] == '0'){
+                //infec[i] = 0;
+                printf("vetor: %s \n", vet);
+                lista[i]->porcentagem = 0;
+            } else if(vet[0] == '1' && vet[1] == '0' && vet[2] == '1' && vet[3] == '0'){
+                //infec[i] = 50;
+                printf("vetor: %s \n", vet);
+                lista[i]->porcentagem = 0;
+            } else if(vet[0] == '1' && vet[1] == '0' && vet[2] == '1' && vet[3] == '1'){
+                //infec[i] = 100;
+                printf("vetor: %s \n", vet);
+                lista[i]->porcentagem = 25;
+            } else if(vet[0] == '1' && vet[1] == '0' && vet[2] == '0' && vet[3] == '1'){
+                //infec[i] = 50;
+                printf("vetor: %s \n", vet);
+                lista[i]->porcentagem = 50;
+            } else {
+                fprintf(stderr, "Erro: ha possibilidades inexistentes para verificacoes!\n");
+            }
+            free(vet);
+            vet = (char*)malloc(4 * sizeof(char));
+            j = 0;
+            
+
+        } else {
+            printf("entrou!\n");
+            vet[j] = aux->val;
+            aux = aux->next;
+            j++;
+        }
+        i++;
+    }
+}*/
