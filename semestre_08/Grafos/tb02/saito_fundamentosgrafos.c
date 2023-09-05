d23f6b27dd12d1b61bdafaf9039be6b44748b10e
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-09-05 18:26:53 -0400

    leitura do arquiv 80%

--- semestre_08/Grafos/tb02/saito_fundamentosgrafos.c
+++ semestre_08/Grafos/tb02/saito_fundamentosgrafos.c
@@ -1,99 +1,112 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
-//-------------------------------LISTA ENCADEADA---------------------------------//
-struct node{
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//---------------------------------------LISTA ENCADEADA---------------------------------------//
+struct node {
     char val;
-    struct node *next;
+    struct node* next;
 };
-struct lista{
-    struct node *begin;
-    struct node *end;
-    char pal[30];
-    int porcent;
+
+struct lista {
+    struct node* begin;
+    struct node* end;
     int size;
 };
-struct node *Node_Create(char val)
-{
-    struct node *node = (struct node *)calloc(1, sizeof(struct node)); //Alocando o No dinamicamente
-    node->val = val; // Atribuindo o parametro passado 
-    node->next = NULL; // Atribuindo nulo ao proximo elemento de um No recem criado
 
+// Funções para criar e manipular a lista encadeada
+struct node *Node_Create(char val) {
+    struct node* node = (struct node*)malloc(sizeof(struct node));
+    node->val = val;
+    node->next = NULL;
     return node;
 }
-struct lista *List_Create()
-{
-    struct lista *lista = (struct lista*)calloc(1, sizeof(struct lista)); // Alocando a lista dinamicamente
-    lista->begin = NULL; // Atribuindo Nulo ao ponteiro pro inicio de uma lista recem criada
-    lista->end = NULL; // Atribuindo Nulo ao ponteiro pro final de uma lista recem criada
-    lista->size = 0;
 
+struct lista *List_Create() {
+    struct lista* lista = (struct lista*)malloc(sizeof(struct lista));
+    lista->begin = NULL;
+    lista->end = NULL;
+    lista->size = 0;
     return lista;
 }
-bool is_Empty(const struct lista *lista)
-{
-    return lista->size == 0;
-}
-void add_Last(struct lista *lista, char val)
-{
-    struct node *new = Node_Create(val);
 
-    //Se a lista estiver vazia
-    if(is_Empty(lista)){
+bool is_Empty(const struct lista* lista) {
+    return lista->size == 0 || lista->begin == NULL;
+}
+
+void add_Last(struct lista* lista, char val) {
+    struct node* new = Node_Create(val);
+
+    if (is_Empty(lista)) {
         lista->begin = lista->end = new;
-    }else{
-        lista->end->next = new; // Proximo elemento do ultimo Node aponta pro novo
-        lista->end = lista->end->next; // O novo Node passa a ser o End
+    } else {
+        lista->end->next = new;
+        lista->end = lista->end->next;
     }
     lista->size++;
 }
-void print(const struct lista *lista)
-{
-    struct node *p = lista->begin;
-    //Enquanto P apontar para um no existente
-    while(p != NULL){
-        printf("%c", p->val);
+
+void print(const struct lista* lista) {
+    struct node* p = lista->begin;
+    while (p != NULL) {
+        printf("%c -> ", p->val);
         p = p->next;
     }
 }
-//-----------------------------------------------------------------------------------------------------------------
-void grafo(struct lista **lista){
-    FILE * file = fopen("arquivo.dot", "r");
 
-    if(file == NULL){
-        printf("ERRO: nao foi possivel abrir o arquivo!\n");
+//------------------------------------------LEITURA DO ARQUIVO------------------------------------------//
+void grafo(struct lista** lista) {
+    FILE* file = fopen("arquivo.dot", "r");
+
+    if (file == NULL) {
+        printf("ERRO: não foi possível abrir o arquivo!\n");
         exit(1);
     }
 
-    int i=0;
+    int i = 0;
     char aux = fgetc(file);
-    while(aux != EOF){
-        aux = fgetc(file);
-        if(aux == '{'){
+    lista[i] = List_Create();
+
+    while (aux != EOF) {
+        if (aux == '{') {
             aux = fgetc(file);
-            while(aux != '}'){
-                if(aux != ' ' && aux != '\n' && aux != ';'){
+            while (aux != '}') {
+                if (aux == '-') {
+                    aux = fgetc(file);  
+                    continue;
+                } else if (aux != ' ' && ((aux >= 'a' && aux <= 'z') || (aux >= 'A' && aux <= 'Z'))) {
                     add_Last(lista[i], aux);
+                    aux = fgetc(file); 
+                    continue;
+                } else if (aux == '\n') {
+                    i++;
+                    lista[i] = List_Create();
+                    aux = fgetc(file); 
+                } else {
+                    aux = fgetc(file); 
                 }
-                aux = fgetc(file);
             }
         } else {
-            continue;
+            aux = fgetc(file); 
         }
     }
+
+    fclose(file);
 }
 
-int main (){
-    struct lista *lista[10];
-    for(int i=0; i<10; i++){
+
+int main() {
+    struct lista** lista = (struct lista**)malloc(10 * sizeof(struct lista*));
+    for (int i = 0; i < 10; i++) {
         lista[i] = List_Create();
     }
     grafo(lista);
-    for(int i=0; i<10; i++){
+    for (int i = 0; i < 10; i++) {
         printf("|%d| ", i);
         print(lista[i]);
         printf("\n");
+        free(lista[i]); // Libera a memória da lista individual
     }
+    free(lista); // Libera a memória do array de ponteiros para listas
     return 0;
 }
