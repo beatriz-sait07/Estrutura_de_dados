e0031eafc0729451ea16b908cbca6ba1e4547c23
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-09-15 23:55:48 -0400

    concluido!

--- semestre_08/Grafos/tb02/beatriz_saito_fundamento_grafos.c
+++ semestre_08/Grafos/tb02/beatriz_saito_fundamento_grafos.c
@@ -14,6 +14,7 @@ struct lista {
     struct node* begin;
     struct node* end;
     int size;
+    char inicio;
 };
 
 struct node* Node_Create(char val) {
@@ -35,7 +36,7 @@ bool is_Empty(const struct lista* lista) {
     return lista->size == 0 || lista->begin == NULL;
 }
 
-void add_Last(struct lista* lista, char val) {
+void insert(struct lista* lista, char val) {
     struct node* new = Node_Create(val);
 
     if (is_Empty(lista)) {
@@ -45,6 +46,17 @@ void add_Last(struct lista* lista, char val) {
         lista->end = lista->end->next;
     }
     lista->size++;
+}
+
+void insert1(struct lista *L, char val)
+{
+    struct node *new = Node_Create(val);
+    new->next = L->begin; // Novo Noh aponta pro Noh de inicio
+    if(is_Empty(L)){
+        L->end = new;
+    }
+    L->begin = new; // Proximo da lista vira o novo Noh
+    L->size++;
 }
 
 void print(const struct lista* lista) {
@@ -61,13 +73,26 @@ void addEdge(struct lista** lista, char vetc1, char vetc2) {
     int index2 = vetc2 - 'a'; // Converte o caractere em um índice
 
     // Adicione vetc2 à lista de vetc1
-    add_Last(lista[index1], vetc2);
+    insert(lista[index1], vetc2);
     // Adicione vetc1 à lista de vetc2 (já que é um gráfico não direcionado)
-    add_Last(lista[index2], vetc1);
+    insert(lista[index2], vetc1);
 }
 
-void grafo(struct lista** lista) {
-    FILE* file = fopen("grafo1.dot", "r");
+void free_List(struct lista** lista, int i) {
+    struct node* p = lista[i]->begin;
+    struct node* aux = NULL;
+    while (p != NULL) {
+        aux = p->next;
+        free(p);
+        p = aux;
+    }
+    free(lista[i]);
+    lista[i] = NULL; // Define a lista como NULL após a desalocação
+    memoria_desalocada = true;
+}
+//------------------------------------- Leitura do arq ---------------------------------------
+void leitura(struct lista** lista) {
+    FILE* file = fopen("grafo3.dot", "r");
 
     if (file == NULL) {
         printf("ERRO: não foi possível abrir o arquivo!\n");
@@ -86,21 +111,7 @@ void grafo(struct lista** lista) {
 
     fclose(file);
 }
-
-void free_List(struct lista** lista, int i) {
-    struct node* p = lista[i]->begin;
-    struct node* aux = NULL;
-    while (p != NULL) {
-        aux = p->next;
-        free(p);
-        p = aux;
-    }
-    free(lista[i]);
-    lista[i] = NULL; // Define a lista como NULL após a desalocação
-    memoria_desalocada = true;
-}
-//------------------------------------- FIM DA LISTA ---------------------------------------
-
+//------------------------------FUNCOES-------------------------------
 //01) Verificar se o grafo é conexo
 void DFS(struct lista** lista, int vertex, bool* visited) {
     visited[vertex] = true;
@@ -124,7 +135,6 @@ bool isConexo(struct lista** lista) {
         visited[i] = false;
     }
 
-    
     int p = -1; // Encontre o primeiro vértice não nulo (se existir)
     for (int i = 0; i < numVertices; i++) {
         if (!is_Empty(lista[i])) {
@@ -230,19 +240,6 @@ void calcularGrauMinimoMaximo(struct lista** lista) {
             grauMaximo = grauVertice;
         }
     }
-    // Imprime o grau mínimo e máximo para cada vértice
-    // for (int i = 0; i < numVertices; i++) { // Assumindo 26 letras do alfabeto
-    //     int grauVertice = 0;
-    //     struct node* p = lista[i]->begin;
-    //     while (p != NULL) {
-    //         grauVertice++;
-    //         p = p->next;
-    //     }
-    //     if (grauVertice > 0) {
-    //         printf("Vértice %c: Grau %d\n", 'a' + i, grauVertice);
-    //     }
-    // }
-    // Imprime o grau mínimo e máximo do grafo
 
     printf("\n05) Grau mínimo do grafo: %d\n", grauMinimo);
     printf("    Grau máximo do grafo: %d\n", grauMaximo);
@@ -349,6 +346,7 @@ bool completo(struct lista** lista) {
     return grafoCompleto;
 }
 
+//10) Verificar se o grafo é regular
 bool grafo_reg(struct lista** lista) {
     int numVertices = 26; // Assumindo que há 26 vértices, 'a' a 'z'
 
@@ -371,13 +369,13 @@ bool grafo_reg(struct lista** lista) {
     return true; // O grafo é regular
 }
 
-void generatePasseios(struct lista** lista, char go, bool* visited, char* caminho, int index, int numVertices) {
+//11) Gerar passeios
+void gerando_passeios(struct lista** lista, char go, bool* visited, char* caminho, int index, int numVertices) {
     visited[go - 'a'] = true; // mantem o controlo dos vértices visitados 
     caminho[index++] = go; 
-
     if (index == numVertices) { 
         for (int i = 0; i < numVertices; i++) {
-            printf("%c ", caminho[i]);
+            printf("%c -> ", caminho[i]);
         }
         printf("\n");
     } else {
@@ -385,14 +383,194 @@ void generatePasseios(struct lista** lista, char go, bool* visited, char* caminh
         while (atual != NULL) {
             char nextVertex = atual->val;
             if (!visited[nextVertex - 'a']) {
-                generatePasseios(lista, nextVertex, visited, caminho, index, numVertices);
+                gerando_passeios(lista, nextVertex, visited, caminho, index, numVertices);
+            }
+            atual = atual->next;
+        }
+    }
+    
+    visited[go - 'a'] = false; 
+}
+void gerando_passeio_total(struct lista** lista, char start, char end, bool* visited, char* path, int index) {
+    visited[start - 'a'] = true; // Marca o vértice atual como visitado
+    path[index++] = start; // Adiciona o vértice atual ao caminho
+
+    if (start == end) { // Se chegarmos ao vértice de destino, imprime o caminho
+        for (int i = 0; i < index; i++) {
+            printf("%c -> ", path[i]);
+        }
+        printf("\n");
+    } else {
+        struct node* current = lista[start - 'a']->begin;
+        while (current != NULL) {
+            char nextVertex = current->val;
+            if (!visited[nextVertex - 'a']) {
+                gerando_passeio_total(lista, nextVertex, end, visited, path, index);
+            }
+            current = current->next;
+        }
+    }
+
+    visited[start - 'a'] = false; // Desmarca o vértice atual como visitado para explorar outros caminhos
+}
+
+//12) Gerar todos os caminhos entre dois vértices
+// Função auxiliar para imprimir uma trilha
+void printTrilha(char* trilha, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%c -> ", trilha[i]);
+    }
+    printf("\n");
+}
+
+// Função recursiva para encontrar trilhas em um grafo
+void generateTrilhas(struct lista** lista, char currentVertex, bool* visited, char* trilha, int index, int numVertices) {
+    visited[currentVertex - 'a'] = true;
+    trilha[index++] = currentVertex;
+
+    if (index >= 2) {
+        // Verifica se a trilha contém um ciclo
+        if (visited[currentVertex - 'a'] && index > 2 && trilha[index - 1] == trilha[0]) {
+            visited[currentVertex - 'a'] = false;
+            return;
+        }
+    }
+
+    if (index == numVertices) {
+        printTrilha(trilha, numVertices);
+    } else {
+        struct node* atual = lista[currentVertex - 'a']->begin;
+        while (atual != NULL) {
+            char nextVertex = atual->val;
+            if (!visited[nextVertex - 'a']) {
+                generateTrilhas(lista, nextVertex, visited, trilha, index, numVertices);
             }
             atual = atual->next;
         }
     }
 
-    visited[go - 'a'] = false; // Marcar o vértice como não visitado para outros passeios
+    visited[currentVertex - 'a'] = false;
 }
+
+// Função para imprimir todas as trilhas possíveis em um grafo
+void encontrar_trilha(struct lista** lista, int numVertices) {
+    bool* visited = (bool*)malloc(sizeof(bool) * numVertices);
+    char* trilha = (char*)malloc(sizeof(char) * numVertices);
+    memset(visited, false, sizeof(bool) * numVertices);
+
+    for (int i = 0; i < numVertices; i++) {
+        generateTrilhas(lista, 'a' + i, visited, trilha, 0, numVertices);
+    }
+
+    free(visited);
+    free(trilha);
+}
+
+//13) Gerar todos os ciclos
+bool Caminho(const char* path, char vertex) {
+    int length = strlen(path);
+    for (int i = 0; i < length; i++) {
+        if (path[i] == vertex) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Função recursiva para encontrar e imprimir todos os circuitos a partir de um vértice
+void encontrou_seu_caminho(struct lista** lista, char start, char current, char* path, int depth) {
+    // Adicione o vértice atual ao caminho
+    path[depth] = current;
+    depth++;
+
+    // Verifique se encontramos um circuito completo
+    if (depth > 2 && current == start) {
+        // Imprima o circuito encontrado
+        printf("Circuito: ");
+        for (int i = 0; i < depth; i++) {
+            printf("%c ", path[i]);
+        }
+        printf("\n");
+    } else {
+        // Continue a busca em profundidade a partir do vértice atual
+        struct node* neighbor = lista[current - 'a']->begin;
+        while (neighbor != NULL) {
+            if (!Caminho(path, neighbor->val)) {
+                encontrou_seu_caminho(lista, start, neighbor->val, path, depth);
+            }
+            neighbor = neighbor->next;
+        }
+    }
+
+    // Remova o vértice atual do caminho antes de retroceder
+    depth--;
+    path[depth] = '\0';
+}
+
+// Função para encontrar e imprimir todos os circuitos em um grafo direcionado
+void caminho_total(struct lista** lista) {
+    int numVertices = 26; // Assumindo que há 26 vértices, 'a' a 'z'
+    char* path = (char*)malloc(numVertices * sizeof(char));
+
+    for (int i = 0; i < numVertices; i++) {
+        if (!is_Empty(lista[i])) {
+            for (int j = 0; j < numVertices; j++) {
+                path[j] = '\0'; // Inicialize o caminho com caracteres nulos
+            }
+            encontrou_seu_caminho(lista, 'a' + i, 'a' + i, path, 0);
+        }
+    }
+
+    free(path);
+}
+
+//14) Função de interface para gerar todos os caminhos entre dois vértices
+void gerar_todos_os_caminhos(struct lista** lista, char start, char end) {
+    int numVertices = 26; // Assumindo que há 26 vértices, 'a' a 'z'
+    bool* visited = (bool*)malloc(numVertices * sizeof(bool));
+    char* path = (char*)malloc(numVertices * sizeof(char));
+
+    for (int i = 0; i < numVertices; i++) {
+        visited[i] = false;
+    }
+
+    printf("Todos os caminhos entre %c e %c:\n", start, end);
+    gerando_passeio_total(lista, start, end, visited, path, 0);
+
+    free(visited);
+    free(path);
+}
+
+void encontrando_caminhos(struct lista** lista, bool* visited, char* path, int* pathLength, char currentVertex, char targetVertex) {
+    int currentIndex = currentVertex - 'a';
+    visited[currentIndex] = true;
+    path[(*pathLength)++] = currentVertex;
+
+    if (currentVertex == targetVertex) {
+        // Imprime o caminho encontrado
+        for (int i = 0; i < *pathLength; i++) {
+            printf("%c", path[i]);
+            if (i < *pathLength - 1) {
+                printf(" -> ");
+            }
+        }
+        printf("\n");
+    } else {
+        struct node* current = lista[currentIndex]->begin;
+        while (current != NULL) {
+            char nextVertex = current->val;
+            int nextIndex = nextVertex - 'a';
+            if (!visited[nextIndex]) {
+                encontrando_caminhos(lista, visited, path, pathLength, nextVertex, targetVertex);
+            }
+            current = current->next;
+        }
+    }
+
+    visited[currentIndex] = false;
+    (*pathLength)--;
+}
+
 
 //-----------------------------------------MAIN-----------------------------------------
 int main() {
@@ -402,17 +580,17 @@ int main() {
         lista[i] = List_Create();
     }
     printf("Lista de adjacencia:\n");
-    grafo(lista);
+    leitura(lista);
     for (int i = 0; i < 26; i++) {
         if (!is_Empty(lista[i])) {
-            printf("%c: ", 'a' + i);
+            insert1(lista[i], 'a' + i);
             print(lista[i]);
             numVertices++;
             printf("\n");
         }
     }
-    printf("--------------------------------------");
-    printf("\n\n01) O grafo eh %s\n", isConexo(lista) ? "conexo" : "nao conexo");
+    printf("-------------------------------------\n");
+    printf("01) O grafo eh %s\n", isConexo(lista) ? "conexo" : "nao conexo");
     printf("\n02) O grafo %s loops\n", loop(lista) ? "tem" : "nao tem");
     printf("\n03) O grafo %s arestas paralelas\n", ArestasParalelas(lista) ? "tem" : "nao tem");
 
@@ -437,18 +615,44 @@ int main() {
 
     printf("\n10) O grafo %s regular\n", grafo_reg(lista) ? "eh" : "NAO eh");
 
+
     printf("\n11) Passeios:\n");
     bool visited[26] = { false };
     char caminho[26];
-
     for (int i = 0; i < numVertices; i++) {
         if (!is_Empty(lista[i])) {
-            printf("%c:\n", 'a' + i);
-            generatePasseios(lista, 'a' + i, visited, caminho, 0, numVertices);
-            printf("\n");
+            
+            gerando_passeios(lista, 'a' + i, visited, caminho, 0, numVertices);
+            printf("---------------------\n");
         }
     }
+
+    printf("\n12) Trilhas possíveis no grafo:\n");
+    encontrar_trilha(lista, numVertices);
     
+    printf("\n14) Caminhos:\n");
+    char startVertex = '\0', endVertex = '\0';   // Vértice de fim
+
+    for (int i = 0; i < 26; i++) {
+        if (!is_Empty(lista[i])) {
+            if (startVertex == '\0') {
+                startVertex = 'a' + i; // Encontre o primeiro vértice não vazio
+            }
+            endVertex = 'a' + i; // O último vértice não vazio será atualizado a cada iteração
+        }
+    }
+
+    if (startVertex != '\0' && endVertex != '\0') {
+        printf("Caminhos entre %c - %c:\n", startVertex, endVertex);
+        gerar_todos_os_caminhos(lista, startVertex, endVertex);
+    } else {
+        printf("Não foi possível encontrar vértices de início e fim.\n");
+    }
+
+    
+    printf("\n---------------------\n");
+    caminho_total(lista);
+
     //-----------------Free-----------------
     for (int i = 0; i < 26; i++) {
         if (!is_Empty(lista[i])) {
@@ -457,6 +661,8 @@ int main() {
             free(lista[i]);
         }
     }
+    
+
     free(lista);
     return 0;
 }
