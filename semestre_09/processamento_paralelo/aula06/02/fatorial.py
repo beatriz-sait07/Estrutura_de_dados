9ceeb19112e99fd38c1e34c89bda50ca2b86cb18
beatriz saito <beatriz.s.gobira@gmail.com>
2024-03-06 14:59:14 -0400

    sem entrada do usuario

--- semestre_09/processamento_paralelo/aula06/02/fatorial.py
+++ semestre_09/processamento_paralelo/aula06/02/fatorial.py
@@ -1,4 +1,5 @@
 from math import factorial
-fat = int(input('numero que deseja o fatorial: '))
+#fat = int(input('numero que deseja o fatorial: '))
+fat = 10
 n = factorial(fat)
 print('{}! = {}'.format(fat, n))
